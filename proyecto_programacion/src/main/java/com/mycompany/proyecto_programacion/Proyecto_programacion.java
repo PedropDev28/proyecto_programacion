ee446b82b002f5dcfc43bd18d90dbc8356c84de3
Pedrop19 <pedromaileducacion@gmail.com>
2022-12-14 17:59:35 +0100

    14/12 PrimerCommit

---
+++ proyecto_programacion/src/main/java/com/mycompany/proyecto_programacion/Proyecto_programacion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyecto_programacion;
+
+/**
+ *
+ * @author LaParca25
+ */
+public class Proyecto_programacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
